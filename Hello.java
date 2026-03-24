a4de1f8199074a3908f2f79f9d70829c411aa9f1
Abdullah <abdullahalhussein25@gmail.com>
2026-03-24 21:07:02 +0100

    Test 1 for ci/cd

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("CI/CD Test Version 2!"); 
+        System.out.println("CI/CD Test Version 3!"); 
     }
 }
 
